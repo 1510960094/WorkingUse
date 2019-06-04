5311c857cfaaf1c78d0991e5a693e7ad7e886f46
1510960094 <1510960094@qq.com>
2019-06-04 18:14:48 +0800

    单例模式

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,8 @@
 import time
 #数据结构与算法python实现
-def fn():
-    begin = time.time()
-    
-    end = time.time()
-    print(begin-end)
+class Singleton(object):
+    def __new__(cls,*args,**kwargs):
+        if not hasattr(cls,'_instance'):
+            orig = super(Singleton,cls)
+            cls._instance = orig.__new__(cls,*args,**kwargs)
+        return cls._instance
